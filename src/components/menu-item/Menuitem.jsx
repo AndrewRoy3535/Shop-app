05976ec7e8ec5e23af0a8c0f0a4e903809a6f9b4
AndrewRoy <eda.twinkle@gmail.com>
2021-02-03 16:34:19 +0600

    menu-items-styles

--- src/components/menu-item/Menuitem.jsx
+++ src/components/menu-item/Menuitem.jsx
@@ -4,7 +4,8 @@ import './Menuitem.scss'
 
 function Menuitem({title, imageUrl, size}) {
     return (
-        <div className={`${size} menu-item`} style={{backgroundImage: `url(${imageUrl})`}}>
+        <div className={`${size} menu-item`}>
+            <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}/>
             <div className='content'>
                 <h1 className='title'>{title}</h1>
                 <span className='subtitle'>shop now</span>
